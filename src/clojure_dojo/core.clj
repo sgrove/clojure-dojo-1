e8913bfc038e92102c0706f82d902b28b40cec5a
Dmitri Akatov <akatov@gmail.com>
2013-02-26 20:47:26 -0800

    first scraper

--- src/clojure_dojo/core.clj
+++ src/clojure_dojo/core.clj
@@ -1,6 +1,17 @@
-(ns clojure-dojo.core)
+(ns clojure-dojo.core
+  (:require [clojure.string :as string])
+  (:use net.cgrand.enlive-html)
+  (:import java.net.URL))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-front-page []
+  (let [uri "http://news.ycombinator.com"
+        url (URL. uri)
+        parsed-html (html-resource url)
+        title-tags (select parsed-html [:.title :a])
+        titles (map (comp first :content) title-tags)
+        point-tags (select parsed-html [:.subtext :span])
+        points (map (comp read-string #(first (string/split % #" ")) first :content) point-tags)
+        time (java.lang.System/currentTimeMillis)
+        points-time-map (map (fn [p] {:time time :points p}) points)
+        ]
+    (zipmap titles points-time-map)))
